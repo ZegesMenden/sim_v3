e3fc30fe5e1efbe3e51bd4e17f4a65d2904bec82
ZegesMenden <camkullberg@gmail.com>
2021-10-10 14:31:37 -0400

    condensed unit tests again at the suggestion of nuff

--- test_unittest.py
+++ test_unittest.py
@@ -4,27 +4,17 @@ import unittest
 class test_vector_math(unittest.TestCase):
     
     def test_add(self):
-
         self.assertEqual( vector3(0.0, 0.0, 0.0) + vector3(1.0, 1.0, 1.0) , vector3(1.0, 1.0, 1.0) )
     
     def test_sub(self):
-
         self.assertEqual( vector3(0.0, 0.0, 0.0) - vector3(1.0, 1.0, 1.0), vector3(-1.0, -1.0, -1.0) )
 
     def test_mul(self):
-
         self.assertEqual( vector3(1.0, 1.0, 1.0) * vector3(2.0, 2.0, 2.0), vector3(2.0, 2.0, 2.0) )
-        self.assertEqual( vector3(1.0, 1.0, 1.0) * vector3(-2.0, -2.0, -2.0), vector3(-2.0, -2.0, -2.0) )
-        self.assertEqual( vector3(-1.0, -1.0, -1.0) * vector3(2.0, 2.0, 2.0), vector3(-2.0, -2.0, -2.0) )
-        self.assertEqual( vector3(-1.0, -1.0, -1.0) * vector3(-2.0, -2.0, -2.0), vector3(2.0, 2.0, 2.0) )
 
     def test_div(self):
-
         self.assertEqual( vector3(1.0, 1.0, 1.0) / vector3(2.0, 2.0, 2.0), vector3(0.5, 0.5, 0.5) )
-        self.assertEqual( vector3(1.0, 1.0, 1.0) / vector3(-2.0, -2.0, -2.0), vector3(-0.5, -0.5, -0.5) )
-        self.assertEqual( vector3(-1.0, -1.0, -1.0) / vector3(2.0, 2.0, 2.0), vector3(-0.5, -0.5, -0.5) )
-        self.assertEqual( vector3(-1.0, -1.0, -1.0) / vector3(-2.0, -2.0, -2.0), vector3(0.5, 0.5, 0.5) )
-
+        
 # class test_quaternion_math(unittest.TestCase):
 
 
