8b701e35e1ddedae56f30bb8f73e1f7ade6a177b
ZegesMenden <camkullberg@gmail.com>
2021-10-10 14:30:08 -0400

    condensed unit testing to not be bad

--- test_unittest.py
+++ test_unittest.py
@@ -5,70 +5,28 @@ class test_vector_math(unittest.TestCase):
     
     def test_add(self):
 
-        self.assertEqual( (vector3(0.0, 0.0, 0.0) + vector3(1.0, 1.0, 1.0)).x , vector3(1.0, 1.0, 1.0).x )
-        self.assertEqual( (vector3(0.0, 0.0, 0.0) + vector3(1.0, 1.0, 1.0)).y , vector3(1.0, 1.0, 1.0).y )
-        self.assertEqual( (vector3(0.0, 0.0, 0.0) + vector3(1.0, 1.0, 1.0)).z , vector3(1.0, 1.0, 1.0).z )
-        
+        self.assertEqual( vector3(0.0, 0.0, 0.0) + vector3(1.0, 1.0, 1.0) , vector3(1.0, 1.0, 1.0) )
     
     def test_sub(self):
 
-        self.assertEqual( (vector3(0.0, 0.0, 0.0) - vector3(1.0, 1.0, 1.0)).x, vector3(-1.0, -1.0, -1.0).x )
-        self.assertEqual( (vector3(0.0, 0.0, 0.0) - vector3(1.0, 1.0, 1.0)).y, vector3(-1.0, -1.0, -1.0).y )
-        self.assertEqual( (vector3(0.0, 0.0, 0.0) - vector3(1.0, 1.0, 1.0)).z, vector3(-1.0, -1.0, -1.0).z )
+        self.assertEqual( vector3(0.0, 0.0, 0.0) - vector3(1.0, 1.0, 1.0), vector3(-1.0, -1.0, -1.0) )
 
     def test_mul(self):
 
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) * vector3(2.0, 2.0, 2.0)).x, vector3(2.0, 2.0, 2.0).x )
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) * vector3(2.0, 2.0, 2.0)).y, vector3(2.0, 2.0, 2.0).y )
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) * vector3(2.0, 2.0, 2.0)).z, vector3(2.0, 2.0, 2.0).z )
-
-        self.assertEqual( (vector3(3.0, 3.0, 3.0) * vector3(2.0, 2.0, 2.0)).x, vector3(6.0, 6.0, 6.0).x )
-        self.assertEqual( (vector3(3.0, 3.0, 3.0) * vector3(2.0, 2.0, 2.0)).y, vector3(6.0, 6.0, 6.0).y )
-        self.assertEqual( (vector3(3.0, 3.0, 3.0) * vector3(2.0, 2.0, 2.0)).z, vector3(6.0, 6.0, 6.0).z )
-
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) * vector3(-2.0, -2.0, -2.0)).x, vector3(-2.0, -2.0, -2.0).x )
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) * vector3(-2.0, -2.0, -2.0)).y, vector3(-2.0, -2.0, -2.0).y )
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) * vector3(-2.0, -2.0, -2.0)).z, vector3(-2.0, -2.0, -2.0).z )
-
-        self.assertEqual( (vector3(3.0, 3.0, 3.0) * vector3(-2.0, -2.0, -2.0)).x, vector3(-6.0, -6.0, -6.0).x )
-        self.assertEqual( (vector3(3.0, 3.0, 3.0) * vector3(-2.0, -2.0, -2.0)).y, vector3(-6.0, -6.0, -6.0).y )
-        self.assertEqual( (vector3(3.0, 3.0, 3.0) * vector3(-2.0, -2.0, -2.0)).z, vector3(-6.0, -6.0, -6.0).z )
-
-        self.assertEqual( (vector3(-1.0, -1.0, -1.0) * vector3(2.0, 2.0, 2.0)).x, vector3(-2.0, -2.0, -2.0).x )
-        self.assertEqual( (vector3(-1.0, -1.0, -1.0) * vector3(2.0, 2.0, 2.0)).y, vector3(-2.0, -2.0, -2.0).y )
-        self.assertEqual( (vector3(-1.0, -1.0, -1.0) * vector3(2.0, 2.0, 2.0)).z, vector3(-2.0, -2.0, -2.0).z )
-
-        self.assertEqual( (vector3(-3.0, -3.0, -3.0) * vector3(2.0, 2.0, 2.0)).x, vector3(-6.0, -6.0, -6.0).x )
-        self.assertEqual( (vector3(-3.0, -3.0, -3.0) * vector3(2.0, 2.0, 2.0)).y, vector3(-6.0, -6.0, -6.0).y )
-        self.assertEqual( (vector3(-3.0, -3.0, -3.0) * vector3(2.0, 2.0, 2.0)).z, vector3(-6.0, -6.0, -6.0).z )
+        self.assertEqual( vector3(1.0, 1.0, 1.0) * vector3(2.0, 2.0, 2.0), vector3(2.0, 2.0, 2.0) )
+        self.assertEqual( vector3(1.0, 1.0, 1.0) * vector3(-2.0, -2.0, -2.0), vector3(-2.0, -2.0, -2.0) )
+        self.assertEqual( vector3(-1.0, -1.0, -1.0) * vector3(2.0, 2.0, 2.0), vector3(-2.0, -2.0, -2.0) )
+        self.assertEqual( vector3(-1.0, -1.0, -1.0) * vector3(-2.0, -2.0, -2.0), vector3(2.0, 2.0, 2.0) )
 
     def test_div(self):
 
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) / vector3(2.0, 2.0, 2.0)).x, vector3(0.5, 0.5, 0.5).x )
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) / vector3(2.0, 2.0, 2.0)).y, vector3(0.5, 0.5, 0.5).y )
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) / vector3(2.0, 2.0, 2.0)).z, vector3(0.5, 0.5, 0.5).z )
+        self.assertEqual( vector3(1.0, 1.0, 1.0) / vector3(2.0, 2.0, 2.0), vector3(0.5, 0.5, 0.5) )
+        self.assertEqual( vector3(1.0, 1.0, 1.0) / vector3(-2.0, -2.0, -2.0), vector3(-0.5, -0.5, -0.5) )
+        self.assertEqual( vector3(-1.0, -1.0, -1.0) / vector3(2.0, 2.0, 2.0), vector3(-0.5, -0.5, -0.5) )
+        self.assertEqual( vector3(-1.0, -1.0, -1.0) / vector3(-2.0, -2.0, -2.0), vector3(0.5, 0.5, 0.5) )
 
-        self.assertEqual( (vector3(4.0, 4.0, 4.0) / vector3(2.0, 2.0, 2.0)).x, vector3(2.0, 2.0, 2.0).x )
-        self.assertEqual( (vector3(4.0, 4.0, 4.0) / vector3(2.0, 2.0, 2.0)).y, vector3(2.0, 2.0, 2.0).y )
-        self.assertEqual( (vector3(4.0, 4.0, 4.0) / vector3(2.0, 2.0, 2.0)).z, vector3(2.0, 2.0, 2.0).z )
+# class test_quaternion_math(unittest.TestCase):
 
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) / vector3(-2.0, -2.0, -2.0)).x, vector3(-0.5, -0.5, -0.5).x )
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) / vector3(-2.0, -2.0, -2.0)).y, vector3(-0.5, -0.5, -0.5).y )
-        self.assertEqual( (vector3(1.0, 1.0, 1.0) / vector3(-2.0, -2.0, -2.0)).z, vector3(-0.5, -0.5, -0.5).z )
-
-        self.assertEqual( (vector3(4.0, 4.0, 4.0) / vector3(-2.0, -2.0, -2.0)).x, vector3(-2.0, -2.0, -2.0).x )
-        self.assertEqual( (vector3(4.0, 4.0, 4.0) / vector3(-2.0, -2.0, -2.0)).y, vector3(-2.0, -2.0, -2.0).y )
-        self.assertEqual( (vector3(4.0, 4.0, 4.0) / vector3(-2.0, -2.0, -2.0)).z, vector3(-2.0, -2.0, -2.0).z )   
-
-        self.assertEqual( (vector3(-1.0, -1.0, -1.0) / vector3(2.0, 2.0, 2.0)).x, vector3(-0.5, -0.5, -0.5).x )
-        self.assertEqual( (vector3(-1.0, -1.0, -1.0) / vector3(2.0, 2.0, 2.0)).y, vector3(-0.5, -0.5, -0.5).y )
-        self.assertEqual( (vector3(-1.0, -1.0, -1.0) / vector3(2.0, 2.0, 2.0)).z, vector3(-0.5, -0.5, -0.5).z )
-
-        self.assertEqual( (vector3(-4.0, -4.0, -4.0) / vector3(2.0, 2.0, 2.0)).x, vector3(-2.0, -2.0, -2.0).x )
-        self.assertEqual( (vector3(-4.0, -4.0, -4.0) / vector3(2.0, 2.0, 2.0)).y, vector3(-2.0, -2.0, -2.0).y )
-        self.assertEqual( (vector3(-4.0, -4.0, -4.0) / vector3(2.0, 2.0, 2.0)).z, vector3(-2.0, -2.0, -2.0).z )    
-
-    
 
 if __name__ == '__main__':
     unittest.main()
