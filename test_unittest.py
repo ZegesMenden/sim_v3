0ae1b29f566cd3e314026e735f9db9fcc54e5aa0
ZegesMenden <camkullberg@gmail.com>
2021-10-10 16:45:37 -0400

    added normalize and length unit tests for the vector3 class, and added quaternion ==, +, -, *, /, conjugation, normalize, euler to quaternion, and quaternion to euler operation unit tests

--- test_unittest.py
+++ test_unittest.py
@@ -2,6 +2,9 @@ from physics import *
 import unittest
 
 class test_vector_math(unittest.TestCase):
+
+    def test_eq(self):
+        self.assertEqual( vector3(0.0, 0.0, 0.0), vector3(0.0, 0.0, 0.0) )
     
     def test_add(self):
         self.assertEqual( vector3(0.0, 0.0, 0.0) + vector3(1.0, 1.0, 1.0) , vector3(1.0, 1.0, 1.0) )
@@ -14,9 +17,38 @@ class test_vector_math(unittest.TestCase):
 
     def test_div(self):
         self.assertEqual( vector3(1.0, 1.0, 1.0) / vector3(2.0, 2.0, 2.0), vector3(0.5, 0.5, 0.5) )
-        
-# class test_quaternion_math(unittest.TestCase):
 
+    def test_norm(self):
+        self.assertEqual( vector3(5.0, 5.0, 5.0).norm().x, vector3(0.5773502691896257, 0.5773502691896257, 0.5773502691896257).x )
+
+    def test_len(self):
+        self.assertEqual( vector3(5.0, 5.0, 5.0).len(), 8.660254037844387 )
+
+class test_quaternion_math(unittest.TestCase):
+
+    def test_eq(self):
+        self.assertEqual( Quaternion(1.0, 0.0, 0.0, 0.0), Quaternion(1.0, 0.0, 0.0, 0.0) )
+    
+    def test_add(self):
+        self.assertEqual( (Quaternion(0.0, 0.0, 0.0, 0.0) + Quaternion(1.0, 1.0, 1.0, 1.0)).w, Quaternion(1.0, 1.0, 1.0, 1.0).w )
+
+    def test_sub(self):
+        self.assertEqual( (Quaternion(1.0, 1.0, 1.0, 1.0) - Quaternion(1.0, 1.0, 1.0, 1.0)).w, Quaternion(0.0, 0.0, 0.0, 0.0).w )
+
+    def test_mul(self):
+        self.assertEqual( Quaternion(1.0,0.5,0.5,0.5) * Quaternion(1.0,0.5,0.5,0.5), Quaternion(0.25,1.0,1.0,1.0) )
+
+    def test_conj(self):
+        self.assertEqual( Quaternion( 1.0, 0.5, 0.5, 0.5 ).conj(), Quaternion( 1.0, -0.5, -0.5, -0.5 ) )
+    
+    def test_norm(self):
+        self.assertEqual( Quaternion(1.0, 0.5, 0.5, 0.5).norm(), Quaternion(0.75592894602, 0.37796447301, 0.37796447301, 0.37796447301) )
+
+    def test_euler_to_quaternion(self):
+        self.assertEqual( Quaternion().eulerToQuaternion(0.174533, 0.174533, 0.174533), Quaternion( 0.9892895259261897, 0.07892647901187541, 0.09406091491321403, 0.07892647901187543 ) )
+
+    def test_quaternion_to_euler(self):
+        self.assertEqual( Quaternion( 0.9892895259261897, 0.07892647901187541, 0.09406091491321403, 0.07892647901187543 ).quaternionToEuler(), vector3( 0.174, 0.174, 0.174 ))
 
 if __name__ == '__main__':
     unittest.main()
