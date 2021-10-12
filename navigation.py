4273d3376aa51b7591a08729fe6fd5bd52b50491
ZegesMenden <camkullberg@gmail.com>
2021-10-12 07:23:57 -0400

    removed datalogging and fixed bugs

--- navigation.py
+++ navigation.py
@@ -29,79 +29,6 @@ class DOF6:
 
         self.rotation_euler = vector3(0.0, 0.0, 0.0) # x is roll, y is pitch, z is yaw
         self.rotaiton_quaternion = Quaternion()
-
-        self.dataLogger = dataLogger()
-
-        self.dataLogger.addDataPoint("time")
-
-        self.dataLogger.addDataPoint("ori_x_roll")
-        self.dataLogger.addDataPoint("ori_y_pitch")
-        self.dataLogger.addDataPoint("ori_z_yaw")
-
-        self.dataLogger.addDataPoint("ori_x_roll_sensed")
-        self.dataLogger.addDataPoint("ori_y_pitch_sensed")
-        self.dataLogger.addDataPoint("ori_z_yaw_sensed")
-
-        self.dataLogger.addDataPoint("reaction_wheel_output_x_roll")
-        self.dataLogger.addDataPoint("actuator_output_y_pitch")
-        self.dataLogger.addDataPoint("actuator_output_z_yaw")
-
-        self.dataLogger.addDataPoint("ori_x_roll_setpoint")
-        self.dataLogger.addDataPoint("ori_y_pitch_setpoint")
-        self.dataLogger.addDataPoint("ori_z_yaw_setpoint")
-
-        self.dataLogger.addDataPoint("ori_x_roll_velocity")
-        self.dataLogger.addDataPoint("ori_y_pitch_velocity")
-        self.dataLogger.addDataPoint("ori_z_yaw_velocity")
-
-        self.dataLogger.addDataPoint("ori_x_roll_velocity_sensed")
-        self.dataLogger.addDataPoint("ori_y_pitch_velocity_sensed")
-        self.dataLogger.addDataPoint("ori_z_yaw_velocity_sensed")
-
-        # self.dataLogger.addDataPoint("ori_x_acceleration")
-        # self.dataLogger.addDataPoint("ori_y_acceleration")
-        # self.dataLogger.addDataPoint("ori_z_acceleration")
-
-        self.dataLogger.addDataPoint("x_position")
-        self.dataLogger.addDataPoint("y_position")
-        self.dataLogger.addDataPoint("z_position")
-
-        self.dataLogger.addDataPoint("x_position_sensed")
-        self.dataLogger.addDataPoint("y_position_sensed")
-        self.dataLogger.addDataPoint("z_position_sensed")
-
-        self.dataLogger.addDataPoint("x_velocity")
-        self.dataLogger.addDataPoint("y_velocity")
-        self.dataLogger.addDataPoint("z_velocity")
-
-        self.dataLogger.addDataPoint("x_velocity_sensed")
-        self.dataLogger.addDataPoint("y_velocity_sensed")
-        self.dataLogger.addDataPoint("z_velocity_sensed")
-        
-        self.dataLogger.addDataPoint("resultant_velocity")
-        self.dataLogger.addDataPoint("resultant_velocity_sensed")
-
-        self.dataLogger.addDataPoint("x_acceleration")
-        self.dataLogger.addDataPoint("y_acceleration")
-        self.dataLogger.addDataPoint("z_acceleration")
-
-        self.dataLogger.addDataPoint("x_acceleration_sensed")
-        self.dataLogger.addDataPoint("y_acceleration_sensed")
-        self.dataLogger.addDataPoint("z_acceleration_sensed")
-
-        self.dataLogger.addDataPoint("x_acceleration_inertial")
-        self.dataLogger.addDataPoint("y_acceleration_inertial")
-        self.dataLogger.addDataPoint("z_acceleration_inertial")
-
-        self.dataLogger.addDataPoint("x_acceleration__inertial_sensed")
-        self.dataLogger.addDataPoint("y_acceleration__inertial_sensed")
-        self.dataLogger.addDataPoint("z_acceleration__inertial_sensed")
-        
-        self.dataLogger.addDataPoint("resultant_acceleration")
-        self.dataLogger.addDataPoint("resultant_acceleration_sensed")
-
-        self.dataLogger.addDataPoint("thrust")
-        self.dataLogger.addDataPoint("mass")
         
     def oriFromEuler(self, x, y, z):
         self.rotation_euler = vector3(x, y, z)
@@ -111,19 +38,31 @@ class DOF6:
         self.accelerationLocal += force / self.mass
 
     def addTorque(self, torque):
-        self.rotationalAcceleration += torque / self.mmoi
+        self.rotationalAcceleration.x += torque.x / self.mmoi.x
+        self.rotationalAcceleration.y += torque.y / self.mmoi.y
+        self.rotationalAcceleration.z += torque.z / self.mmoi.z
 
     def update(self, dt):
         
-        self.rotationalVelocity += self.rotationalAcceleration
+        self.rotationalVelocity += (self.rotationalAcceleration) * dt
+        self.rotationalVelocity.x = 0.0
         self.rotaiton_quaternion.updateOrientation(self.rotationalVelocity.x, self.rotationalVelocity.y, self.rotationalVelocity.z, dt)
         self.rotation_euler = self.rotaiton_quaternion.quaternionToEuler()
 
-        self.accelerationInertial = self.rotaiton_quaternion.rotateVector(self.accelerationLocal.x, self.accelerationLocal.y, self.accelerationLocal.z)
+        self.accelerationInertial = self.rotaiton_quaternion.rotateVector(vector3(self.accelerationLocal.x, self.accelerationLocal.y, self.accelerationLocal.z))
         self.accelerationInertial += self.gravity
 
         self.velocityInertial += self.accelerationInertial * dt
         self.positionInertial += self.velocityInertial * dt
+
+        if self.positionInertial.x <= 0:
+            self.velocityInertial = vector3(0.0, 0.0, 0.0)
+            self.rotationalVelocity = vector3(0.0, 0.0, 0.0)
+            self.positionInertial = vector3(0.0, self.positionInertial.y, self.positionInertial.z)
+
+        self.rotationalAcceleration = vector3(0.0, 0.0, 0.0)
+        self.accelerationInertial = vector3(0.0, 0.0, 0.0)
+        self.accelerationLocal = vector3(0.0, 0.0, 0.0)
 
         # self.dataLogger.recordVariable("time", self.time)
         # self.dataLogger.recordVariable("ori_x_roll", self.rotation_euler.x)
@@ -144,9 +83,9 @@ class DOF6:
         # self.dataLogger.recordVariable("ori_x_roll_velocity_sensed", self.)
         # self.dataLogger.recordVariable("ori_y_pitch_velocity_sensed", self.)
         # self.dataLogger.recordVariable("ori_z_yaw_velocity_sensed", self.)
-        # self.dataLogger.recordVariable("ori_x_acceleration", self.)####", self.)
-        # self.dataLogger.recordVariable("ori_y_acceleration", self.)####", self.)
-        # self.dataLogger.recordVariable("ori_z_acceleration", self.)####", self.)
+        # # self.dataLogger.recordVariable("ori_x_acceleration", self.)####", self.)
+        # # self.dataLogger.recordVariable("ori_y_acceleration", self.)####", self.)
+        # # self.dataLogger.recordVariable("ori_z_acceleration", self.)####", self.)
         # self.dataLogger.recordVariable("x_position", self.)
         # self.dataLogger.recordVariable("y_position", self.)
         # self.dataLogger.recordVariable("z_position", self.)
