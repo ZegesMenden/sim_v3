20e9cc3c118bcd80a34f09eb4f7100f2def2b1f7
ZegesMenden <camkullberg@gmail.com>
2021-11-04 10:34:45 -0400

    merged dataManagement.py
    merged datamanagement and datavisualisation

--- dataManagement.py
+++ dataManagement.py
@@ -99,7 +99,39 @@ class dataLogger:
             return self.currentLog[str(variableName)]
         else:
             raise IndexError("datapoint not initialized")
-    
+
+class dataVisualiser:
+    def __init__(self):
+        self.allDataDescriptions = []
+
+    def graph_from_csv(self, datapoints):
+        descriptionNum = 0
+        pointsToLog = []
+
+        for description in self.allDataDescriptions:
+            for requestedDatapoint in datapoints:
+                if str(description) == str(requestedDatapoint):
+                    pointsToLog.append(descriptionNum)
+            descriptionNum += 1
+        
+        with open('data_out.csv', newline='\n') as pathFile:
+            reader = csv.reader(pathFile, delimiter=',', quotechar='"')
+            logList = []
+            dataOut = []
+            for index, row in enumerate(reader):    
+                for point in pointsToLog:
+                    logList.append(row[point])
+                if index == 0:
+                    for x in logList:
+                        dataOut.append([])
+
+                if index > 0:
+                    for index, point in enumerate(dataOut):
+                        point.append(float(row[pointsToLog[index]]))
+                logList = []
+        
+        return dataOut
+
 class flightPath:
 
     def __init__(self):
